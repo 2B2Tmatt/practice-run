f1485bd9989d694caa6aa521d4f91a4b4f9960e7
Matt Ge <mattyige@gmail.com>
2025-11-01 18:22:42 -0400

    smile branch changes

--- test1.py
+++ test1.py
@@ -2,3 +2,10 @@ for i in range(10):
     print(i)
 
 print("this was made from the test1 branch")
+
+
+print("I love merging") 
+
+print("I don't love issues when merging") 
+
+print("That's why im here, trying to figure all this out") 
